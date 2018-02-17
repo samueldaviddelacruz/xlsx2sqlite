34a54e60eae39ae37ecd5427a4ab6301898ffa71
Samuel David De La Cruz <alphaelena@gmail.com>
2018-02-16 21:21:00 -0400

    Add createTable test and its implementation.

--- test/index.test.js
+++ test/index.test.js
@@ -1,21 +1,67 @@
 
 const chai = require('chai')
-const xlsx2Sqlite = require('../lib/index');
+const { createDatabase, createTable } = require('../lib/index');
 
-const fs =  require('fs');
+const fs = require('fs');
 const should = chai.should()
 
 
+const fileName = "employee"
+const extension = "sqlite3"
+const columns = [
+    { 'columnName': 'name', type: 'TEXT' }
+    ,{ 'columnName': 'lastName', type: 'TEXT' }
+    ,{ 'columnName': 'age', type: 'INTEGER' },
+    { 'columnName': 'salary', type: 'REAL' }]
+const tableName = "employee"
+describe('main functions', () => {
+    before(function () {
 
-describe('createDatabase', ()=>{
-    
-    it('should create a database with a given string name',()=>{
-        const fileName = "person.db"
-        const createdDb = xlsx2Sqlite.createDatabase(fileName);
-        
-        createdDb.should.exist
+        if (fs.existsSync(`${fileName}.${extension}`)) {
 
-        fs.existsSync(fileName).should.equal(true)
-        
+            fs.unlinkSync(`${fileName}.${extension}`)
+        }
+        // runs before all tests in this block
     });
+
+    describe('database creation', () => {
+
+        describe('createDatabase', () => {
+
+            it('should create a database with a given string name', () => {
+
+                const createdDb = createDatabase(fileName);
+
+                createdDb.should.exist
+
+                fs.existsSync(`${fileName}.${extension}`).should.equal(true)
+
+            });
+
+        });
+
+    })
+
+
+
+    describe('table creation', () => {
+
+        describe('createTable', () => {
+
+            it('should create a table in an open database', () => {
+
+
+                const createdDb = createDatabase(fileName);
+                const dbChanges = createTable(createdDb, tableName, columns);
+                dbChanges.changes.should.exist;
+
+            });
+        });
+
+    })
+
 })
+
+
+
+

7804c2daaf75cb8c267545a7b9d970e49aef6fe4
Samuel David De La Cruz <samy.David@clevertech.biz>
2018-04-29 00:42:02 -0400

    Adds mapRowsToSqliteColumns utility function and tests.

--- test/Util.test.js
+++ test/Util.test.js
@@ -1,40 +1,33 @@
-
-const {mapRowsToSqliteColumns,getSheetsFromExcelWorkBook} = require('../lib/Util/Utils')
-const chai = require('chai');
-const xlsx = require('xlsx');
-const fs = require('fs');
+const {
+  mapRowsToSqliteColumns,
+  getSheetsFromExcelWorkBook
+} = require("../lib/Util/Utils");
+const chai = require("chai");
+const xlsx = require("xlsx");
+const fs = require("fs");
 
 const should = chai.should();
 
-describe('util functions', () => {
-    describe('getSheetsFromWorkBook',()=>{
+describe("util functions", () => {
+  describe("getSheetsFromWorkBook", () => {
+    it("should return an array of sheets", () => {
+      let sheets = [];
+      sheets = getSheetsFromExcelWorkBook("test.xlsx");
+
+      sheets.length.should.be.greaterThan(0);
+    });
+  });
+  describe("mapRowsToSqliteColumns", () => {
+    it("should return an Array of sql types columns", async () => {
+      const sheetJsons = getSheetsFromExcelWorkBook("test.xlsx").map(data => {
+        return {
+          sheetName: data.name,
+          jsonRows: xlsx.utils.sheet_to_json(data.sheet, { defval: "" })
+        };
+      });     
+      const columns = mapRowsToSqliteColumns(sheetJsons[0].jsonRows);
+      columns.length.should.be.greaterThan(0);
       
-       it('should return an array of sheets',()=>{
-         let sheets = [];
-          sheets = getSheetsFromExcelWorkBook('test.xlsx');
-
-          sheets.length.should.be.greaterThan(0)
-       })
-
-        
-    })
-    describe('mapRowsToSqliteColumns',()=>{
-
-        it('should return an Array of sql types columns',async () =>{
-            
-       
-            const sheetJsons = getSheetsFromExcelWorkBook('test.xlsx').map( (data) => {
-                return {sheetName:data.name, jsonRows:xlsx.utils.sheet_to_json(data.sheet,{defval:""})}
-            }  );
-            console.log(sheetJsons)
-            /*
-            const columns = mapRowsToSqliteColumns();
-            columns.length.should.be.greaterThan(0)
-            */
-        })
-
-
-    })
-
-
-})
+    });
+  });
+});
